16860be35087e181efa4a5ca8a3fb4ede3cfa000
Stanli Regala <stanliregala@gmail.com>
2025-07-26 20:41:25 +0800

    cleanup old audio files

--- app.py
+++ app.py
@@ -67,6 +67,13 @@ def get_model_files():
     
     return sorted(model_files, key=lambda x: x["filename"])
 
+def cleanup_old_audio_files(max_age_hours=1):
+    audio_dir = Path("static/audio")
+    cutoff_time = time.time() - (max_age_hours * (60 * 60))
+
+    for audio_file in audio_dir.glob("tts_output_*.wav"):
+        if audio_file.stat().st_mtime < cutoff_time:
+            audio_file.unlink()
 
 # Webpage routes
 @app.route('/')
@@ -250,6 +257,9 @@ def api_generate():
         file_size = output_path.stat().st_size
         
         logger.info(url_for('serve_audio', filename=filename))
+
+        # Delete old audio files
+        cleanup_old_audio_files(max_age_hours=1)
 
         return {
             "success": True,
